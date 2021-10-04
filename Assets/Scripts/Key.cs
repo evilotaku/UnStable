a74bf6da8cb3eff15bfbce6564480352b1b1bc46
zuran <gsugrader@gmail.com>
2021-10-03 21:39:19 -0700

    Added key model

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -6,17 +6,17 @@ public class Key : MonoBehaviour
 {
   public GameObject Player;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  // Start is called before the first frame update
+  void Start()
+  {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    gameObject.transform.Rotate(new Vector3(1, 1, 0));
+  }
 
   private void OnTriggerEnter(Collider other)
   {
