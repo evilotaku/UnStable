9f323a7877f61e728e59877bcf502ffc465452fd
zuran <gsugrader@gmail.com>
2021-10-03 21:23:09 -0700

    Added stable unlock with key

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,19 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerManager : MonoBehaviour
 {
   public bool HasKey = false;
+  private SphereCollider _FreedomRadius;
     // Start is called before the first frame update
     void Start()
     {
-        
+    _FreedomRadius = GetComponent<SphereCollider>();
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //if(_FreedomRadius.enabled) _FreedomRadius.enabled = false;
+  }
+
+  public void OnUseKey(InputValue value)
+  {
+    if (!HasKey) return;
+    _FreedomRadius.enabled = true;
+    StartCoroutine(DisableFreedomRadius());
+  }
+
+  public void OnTriggerEnter(Collider other)
+  {
+    Follower follower = other.gameObject.GetComponent<Follower>();
+    if (follower != null)
+    {
+      follower.IsPenned = false;
     }
+  }
+
+  private IEnumerator DisableFreedomRadius()
+  {
+    yield return new WaitForSeconds(1);
+    _FreedomRadius.enabled = false;
+  }
 }
